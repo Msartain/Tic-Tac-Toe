32ce95255809fa3158dbc1f528ff5637d1b84c3f
Matthew Sartain <matthewsartain@me.com>
2019-10-25 15:04:21 -0500

    built some html and started on js functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
 console.log("ready to go");
+
+/*----------constants--------*/
+const playerOne = "x" ;
+const playerTwo = "o" ;
+
+
+
+
+/*---------app's state (variables)-------*/
+
+let playerOneMvs, playerTwoMvs, totalMvs
+
+
+
+/*-----cached element references--------*/
+document.getElementById('board').addEventListener('click', handleBoxClick);
+
+// document.getElementById('reset').addEventListener('click' init);
+
+
+
+/*--------event listeners-------*/
+
+
+
+
+
+/*----------functions-----------*/
+
+
+function handleBoxClick(evt){
+    evt.target.textContent = playerOne;
+    console.log(evt.target.textContent);
+}
+
+function init(evt){
+
+};
