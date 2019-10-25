5e817684970ad2f5b96f2d0070754e9ba565992f
Matthew Sartain <matthewsartain@me.com>
2019-10-25 16:51:59 -0500

    created variables for each box id

--- js/main.js
+++ js/main.js
@@ -16,7 +16,15 @@ let playerOneMvs, playerTwoMvs, totalMvs
 document.getElementById('board').addEventListener('click', handleBoxClick);
 
 // document.getElementById('reset').addEventListener('click' init);
-let box1 = document.getElementById('box1')
+let box1 = document.getElementById('box1');
+let box2 = document.getElementById('box2');
+let box3 = document.getElementById('box3');
+let box4 = document.getElementById('box4');
+let box5 = document.getElementById('box5');
+let box6 = document.getElementById('box6');
+let box7 = document.getElementById('box7');
+let box8 = document.getElementById('box8');
+let box9 = document.getElementById('box9');
 
 
 /*--------event listeners-------*/
