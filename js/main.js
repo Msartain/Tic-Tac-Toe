86322d100bb4fa8c902b76c10141f87340b47a02
Matthew Sartain <matthewsartain@me.com>
2019-10-28 21:44:14 -0500

    add winner/tie message, extra styling

--- js/main.js
+++ js/main.js
@@ -12,15 +12,21 @@ let playerOneTotalScore = 0;
 let playerTwoTotalScore = 0;
 
 /*-----cached element references--------*/
+// let boxContent = document.querySelector('td');
+// boxContent.style.color = 'white';
 
+// playerOne.style.color = 'white';
 
+let message = document.querySelector('h3');
+message.style.color = '#CA0146'
+message.style.fontFamily= 'Courier New, Courier, monospace';
+message.style.
 /*--------event listeners-------*/
 
 document.getElementById('board').addEventListener('click', handleBoxClick);
 
 document.getElementById('reset').addEventListener('click', init);
 
-// document.getElementsByTagName('td').style.fontSize = '3vh';
 /*----------functions-----------*/
 init();
 
@@ -51,6 +57,7 @@ function preventDoubleClick(click, evt){
 function checkWinner(box, arr, score){
         arr.push(box);
        if ((arr.includes("1") && arr.includes("2") && arr.includes("3")) ||
+           (arr.includes("4") && arr.includes("5") && arr.includes("6")) ||
            (arr.includes("7") && arr.includes("8") && arr.includes("9")) ||
            (arr.includes("1") && arr.includes("5") && arr.includes("9")) ||
            (arr.includes("3") && arr.includes("5") && arr.includes("7")) ||
@@ -58,11 +65,12 @@ function checkWinner(box, arr, score){
            (arr.includes("2") && arr.includes("5") && arr.includes("8")) ||
            (arr.includes("3") && arr.includes("6") && arr.includes("9"))){
             score++;
-            window.setTimeout(window.alert, 350, "Win!");
+            message.innerHTML = '<h2>Winner!</h2>';
+            // window.setTimeout(window.alert, 350, "Win!");
         }if(boxesClicked.length === 9) {
-            window.setTimeout(window.alert, 350, "Tie Game.");
+            message.innerHTML = '<h2>Tie Game!</h2>';
+            // window.setTimeout(window.alert, 350, "Tie Game.");
         }
-        // console.log("win win" + playerOneTotalScore);
     };
 
 //initialize game 
@@ -72,6 +80,10 @@ function init(){
     playerOneMvs = [];
     playerTwoMvs = [];
     for(let i = 1; i <= 9; i++){
-    document.getElementById(i.toString()).textContent = '';
+        document.getElementById(i.toString()).textContent = '';
     };
 }; 
+
+
+
+tststkgjdi
